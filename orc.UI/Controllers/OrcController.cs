30d670718f6ac412adb464f629fabc0db7088412
mazen14200 <mazen142000@yahoo.com>
2025-06-29 17:59:38 +0300

    added front and make first small example of ocr

---
+++ orc.UI/Controllers/OrcController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
+using System;
+using Tesseract;
+
+
+namespace orc.UI.Controllers
+{
+    [Route("[controller]")]
+    public class OrcController : Controller
+    {
+        [HttpGet("Orc")]
+        public IActionResult Orc()
+        {
+            return View();
+        }
+        [HttpPost("Orc")]
+        public async Task<IActionResult> Orc(string firstName, string lastName, IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var filePath = Path.Combine("wwwroot/uploads", Path.GetFileName(file.FileName));
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+                Console.WriteLine("النص المستخرج:");
+
+                //string imagePath = "D:\\mazen\\orc\\orc.UI\\wwwroot\\uploads\\ITFdiag1.png"; // مسار الصورة
+                string imagePath = filePath;
+                string tessDataPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "tessdata");
+
+                string extractedText = ExtractTextFromImage(imagePath, tessDataPath);
+                Console.WriteLine(extractedText);
+
+                ViewBag.Message = "File uploaded successfully!";
+            }
+            else
+            {
+                ViewBag.Message = "No file uploaded!";
+            }
+
+            //return View("UploadResult");
+            return View();
+        }
+
+        static string ExtractTextFromImage(string imagePath, string tessDataPath)
+        {
+            using var engine = new TesseractEngine(tessDataPath, "eng", EngineMode.Default);
+            using var img = Pix.LoadFromFile(imagePath);
+            using var page = engine.Process(img);
+            return page.GetText();
+        }
+    }
+}
--- orc.UI/Program.cs
+++ orc.UI/Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
